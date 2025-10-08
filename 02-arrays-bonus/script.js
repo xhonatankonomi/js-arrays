dcc65e0c745285241a40f617928c3debc66a7526
xhonatankonomi <xhoni8962@gmail.com>
2025-10-08 15:57:08 +0200

    feat: Implement array manipulation exercises
    This commit implements the array manipulation exercises:
    - Reverses the order of teachers in the `teachers` array and stores the result in `reversedTeachers`.
    - Creates an array `longNames` containing teachers with names of length 5 or greater.

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,7 +10,8 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.reverse();
+console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
