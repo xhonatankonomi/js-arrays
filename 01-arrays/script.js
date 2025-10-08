9851262100e67d2d0835eb2ccae5a29b9220d70f
xhonatankonomi <xhoni8962@gmail.com>
2025-10-08 15:18:55 +0200

    feat: Access array elements by index
    This commit accesses the array element at index 3 and assigns it to the fourthTeacher variable. It also logs the fourthTeacher to the console to verify the change.

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,7 +10,8 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3];
+console.log(fourthTeacher)
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
