a102e80362e755249deac4f7b5471e903f40c08b
xhonatankonomi <xhoni8962@gmail.com>
2025-10-08 15:26:17 +0200

    feat: Remove last teacher from the array
    This commit removes the last teacher from the `teachers` array using the `pop()` method and stores it in the `lastTeacher` variable. A console log was added to display the value of `lastTeacher`.

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -18,7 +18,8 @@ teachers[4] = "Patrick"
 console.log(teachers[4])
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
