23f2ba0b53a07ea5f17b3f1f25e1dac4d0910465
xhonatankonomi <xhoni8962@gmail.com>
2025-10-08 15:35:48 +0200

    feat: Demonstrate array manipulation techniques
    This commit modifies the `teachers` array to illustrate common
    array manipulation techniques including accessing elements by
    index, replacing elements, removing elements using `pop()` and
    `shift()`, adding elements to the beginning and end, and
    finding the index of an element.

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,23 +11,27 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-console.log(fourthTeacher)
+console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers[4] = "Patrick"
-console.log(teachers[4])
+teachers[4] = "Patrick";
+console.log(teachers[4]);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-console.log(lastTeacher)
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
-console.log(firstTeacher)
+console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
+const vanessa = teachers.push("Vanessa");
+console.log(teachers[5]);
+
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
