f161c186ff101cb3a0a013f320cab845f62db411
xhonatankonomi <xhoni8962@gmail.com>
2025-10-08 15:31:15 +0200

    feat: Remove first teacher and log to console
    Removes the first teacher from the `teachers` array using `shift()` and logs the removed teacher to the console. This fulfills task #4 of the array exercises.

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -22,7 +22,8 @@ const lastTeacher = teachers.pop();
 console.log(lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
