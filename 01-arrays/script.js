5d3483602b0d06564aa77e204e058d9a12321e87
xhonatankonomi <xhoni8962@gmail.com>
2025-10-08 15:20:08 +0200

    feat: Replace fifth teacher and log to console
    Replaces the fifth teacher in the `teachers` array with "Patrick" and logs the change to the console. This fulfills the array manipulation exercise.

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -14,6 +14,8 @@ const fourthTeacher = teachers[3];
 console.log(fourthTeacher)
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = "Patrick"
+console.log(teachers[4])
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
